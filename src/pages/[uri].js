55c1ac9652938c3d007632f9d5a2876653461224
JoseIdrovo <114527497+JoseIdrovo@users.noreply.github.com>
2023-02-28 17:41:53 -0500

    Finalized Linking of Next.js and Wordpress
    Added footer and header to slug pages

--- src/pages/[uri].js
+++ src/pages/[uri].js
@@ -1,17 +1,29 @@
-import { getPostByUri } from "lib/test-data";
 import { client } from 'lib/apollo'
 import { gql } from "@apollo/client"
+import styles from '@/styles/Home.module.css'
+import { Header } from "../common/components/ui/header/header.js"
+import { Footer } from "../common/components/ui/footer/footer.js"
 
 export default function SlugPage({ post }) {
     return (
+        <main className={styles.main}>
+        <div className="page-wrapper">
         <div>
-            <p> Bruh </p>
+            <Header/>
+            <div className={styles.fullWidthImage}> Bruh</div>
+            <h1>{post.title}</h1>
+            <div dangerouslySetInnerHTML={{__html: post.content}}></div>
+          <Footer />
         </div>
+        </div>
+       
+      </main>
+
     )
 }
 
 export async function getStaticProps({ params }){
-    const GET_POST_BY_URI = gql`
+    const GETPOSTBYURI = gql`
         query GetPostByURI($id: ID!) {
             post(id: $id, idType: URI) {
             title
@@ -28,11 +40,12 @@ export async function getStaticProps({ params }){
         }
     `
     const response = await client.query({
-        query: GET_POST_BY_URI,
+        query: GETPOSTBYURI,
         variables: {
             id: params.uri
         }
     })
+
     const post = response?.data?.post
     return {
         props: {
@@ -42,7 +55,7 @@ export async function getStaticProps({ params }){
 }
 
 export async function getStaticPaths(){
-    const paths = []
+    const paths = [];
     return {
         paths,
         fallback: 'blocking'
