96f0bf951a4e3af376b77d1509c00d7acdb606d5
Adrian Rodriguez @ RPI '24 <arodr517@gmail.com>
2023-03-24 22:03:11 -0400

    Recreate DoubleCircle Component in NextJs

--- src/common/components/modules/section-three/section.component.jsx
+++ src/common/components/modules/section-three/section.component.jsx
@@ -6,20 +6,24 @@ import Image from 'next/image'
 
 import MultiHeader from "../../ui/multi-header.jsx"
 import Button from "../../ui/button.jsx"
+import { DoubleCircle } from "../../ui/doubleCircle/doubleCircle"
 
 import { SectionThreeHeader, SectionThreeSubHeader, SectionThreeParagraph1, SectionThreeParagraph2 } from '@/text/homepage'
 
 const SectionThree = (props) => {
     return (
-        <div className={styles['section-content-container']}>
-            <div className={styles['section-first-container'] + ' saratoga-half-container'}>
-                <MultiHeader subText={SectionThreeSubHeader} >{ SectionThreeHeader }</MultiHeader> 
-                <p>{SectionThreeParagraph1}</p>
-                <p>{SectionThreeParagraph2}</p>
-                <Button text={'Learn More'}></Button>
+        <div id={styles.ContentContainer}>
+            <div id={styles.SectionTwoThirdContainer}>
+                <div id={styles.SectionWrapper}>
+                    <div id={styles.SectionPreHeaderText}>
+                        {SectionThreeHeader}
+                    </div>
+                    <h2> {SectionThreeSubHeader} </h2>
+                    <p> {SectionThreeParagraph1} </p>
+                </div>
             </div>
-            <div className={styles['section-second-container']  + ' saratoga-half-container'}>
-
+            <div id={styles.SectionOneThirdContainer}>
+                <DoubleCircle circle1-size={"m"} circle2-size={"l"}/>
             </div>
         </div>
     
